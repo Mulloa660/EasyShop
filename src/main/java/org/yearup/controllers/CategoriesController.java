c2b6d816a001c987293594e3a6c66f90b387588e
Michelle Ulloa <umichelle244@gmail.com>
2023-06-27 10:07:11 -0400

    Finished controller getById controller in CategoriesController class

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -52,8 +52,23 @@ public class CategoriesController {
 
     // add the appropriate annotation for a get action
     public Category getById(@PathVariable int id) {
+        {
+            try
+            {
+                var category = categoryDao.getById(id);
+
+                if(category == null)
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+                return category;
+            }
+            catch(Exception ex)
+            {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+            }
+        }
         // get the category by id
-        return null;
+
     }
 
     // the url to return all products in category 1 would look like this
