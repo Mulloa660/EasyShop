50df0ad6de4433f062acd722ffa85c7ceb63fa47
Michelle Ulloa <umichelle244@gmail.com>
2023-06-27 10:04:03 -0400

    Finished controller for getAllCategories method

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,48 +1,57 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 
+import javax.annotation.security.PermitAll;
 import java.util.List;
 
 // add the annotations to make this a REST controller
 // add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
+// http://localhost:8080/categories
 // add annotation to allow cross site origin requests
 @RestController
 @RequestMapping("categories")
 @CrossOrigin
-public class CategoriesController
-{
+public class CategoriesController {
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
 
     // create an Autowired controller to inject the categoryDao and ProductDao
-@Autowired
-public CategoriesController(CategoryDao mySqlCategoryDao) {this.categoryDao = mySqlCategoryDao; }
+    @Autowired
+    public CategoriesController(CategoryDao mySqlCategoryDao) {
+        this.categoryDao = mySqlCategoryDao;
+    }
 
     // add the appropriate annotation for a get action
 
-    @GetMapping
+    @GetMapping("")
+    @PreAuthorize("PermitAll()")
     //@RequestMapping(path = "/books", method = RequestMethod.GET)
     public List<Category> getAllCategories(@RequestParam(name = "category_id", required = false) int categoryId,
-                                  @RequestParam(name = "name", required = false) String name,
+                                           @RequestParam(name = "name", required = false) String name,
                                            @RequestParam(name = "description", required = false) String description) {
         // find and return all categories
-        return categoryDao.getAllCategories(categoryId, name, description);
-
-        // find and return all categories
+        {
+            try {
+                return categoryDao.getAllCategories(categoryId, name, description);
+            } catch (Exception ex) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+            }
+        }
 
     }
 
     // add the appropriate annotation for a get action
-    public Category getById(@PathVariable int id)
-    {
+    public Category getById(@PathVariable int id) {
         // get the category by id
         return null;
     }
@@ -50,32 +59,28 @@ public CategoriesController(CategoryDao mySqlCategoryDao) {this.categoryDao = my
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
     @GetMapping("{categoryId}/products")
-    public List<Product> getProductsById(@PathVariable int categoryId)
-    {
+    public List<Product> getProductsById(@PathVariable int categoryId) {
         // get a list of product by categoryId
         return null;
     }
 
     // add annotation to call this method for a POST action
     // add annotation to ensure that only an ADMIN can call this function
-    public Category addCategory(@RequestBody Category category)
-    {
+    public Category addCategory(@RequestBody Category category) {
         // insert the category
         return null;
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
-    public void updateCategory(@PathVariable int id, @RequestBody Category category)
-    {
+    public void updateCategory(@PathVariable int id, @RequestBody Category category) {
         // update the category by id
     }
 
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
-    public void deleteCategory(@PathVariable int id)
-    {
+    public void deleteCategory(@PathVariable int id) {
         // delete the category by id
     }
 }
